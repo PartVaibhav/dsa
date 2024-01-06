c759e85d0f98690214bd217ff546c76b98813e1c
Vaibhav Suryavanshi <vaibhavsp143@gmail.com>
2024-01-06 23:08:58 +0530

    Corrected code

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -3,34 +3,35 @@ using namespace std;
 
 int binarySearch(int arr[], int size, int key)
 {
-    int start = arr[0];
+    int start = 0;
     int end = size - 1;
     int mid = (start + (end-start)/2);
 
-    while(start >= end)
+    while(start <= end)
     {
-        if(mid == key)
+        if(arr[mid] == key)
         {
             return mid;
         }
-        if(key > mid)
-        {
-            end = mid - 1;
-        }
-        else
+        if(key > arr[mid])
         {
             start = mid + 1;
         }
+        else
+        {
+            end = mid - 1;
+        }
         
-        int mid = (start + (end-start)/2);
+        mid = (start + (end-start)/2);
     }
+    return -1;
 }
 
 
 int main() {
    int arr[6] = {2,4,6,7,9,11};
     
-    int search = binarySearch(arr,6,6);
+    int search = binarySearch(arr,6,7);
     cout << "Index is " << search << endl;
 
     return 0;
